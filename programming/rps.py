8659b955ccaf0e663b801e0e51c8318b87883b89
aled1027 <a.led1027@gmail.com>
2015-05-25 15:46:08 -0700

    second commit

--- programming/rps.py
+++ programming/rps.py
@@ -14,9 +14,9 @@ c = s.abi_contract('rps.se')
 
 """
 -1 ~ error
-0 ~ tie
+0 ~ player 0
 1 ~ player 1
-2 ~ player 2
+2 ~ tie
 """
 
 ##################################### UTILITY FUNCTIONS ########################################
@@ -28,59 +28,42 @@ def hex_to_byte_string(arg):
 ##################################### SETUP COMMITMENTS ########################################
 choice = ["rock", "paper", "scissors"]
 
+# bs stands for byte string
+choice0 = random.randint(0,2)
+choice0_bs = hex_to_byte_string(choice0)
+nonce0_bs = hex_to_byte_string(0x01)
 
+choice1 = random.randint(0,2)
+choice1_bs = hex_to_byte_string(choice1)
+nonce1_bs = hex_to_byte_string(0x01)
 
-#0 = rock; 1 = paper; 2 = scissors
-choice1 = 0x01
-nonce1 = 0x01
-
-ch1 = hex_to_byte_string(choice1)
-no1 = hex_to_byte_string(nonce1)
-
-print("Player zero chooses {} which is: {}").format(choice1, choice[choice1])
-
-# this i'm iffy on
-k0_pub_addr_hex = utils.encode_hex(tester.k0)
+print("Player zero chooses %d which is: %s" % (choice0, choice[choice0]))
+print("Player one chooses %d which is: %s" % (choice1, choice[choice1]))
 
 ## Prepare and pad the address
+# TODO looks like tester.k0 is getting encoded into hex twice.
+# TODO why is this being cast into a long?
+k0_pub_addr_hex = utils.encode_hex(tester.k0)
 k0_pub_addr = hex_to_byte_string(long(k0_pub_addr_hex.encode('hex'), 16))
-# k0_pub_addr  = ''.join(map(chr, tobytearr(long(k0_pub_addr_hex.encode('hex'),16),32)))
-
-## Now use it for the commitment
-s1 = ''.join([k0_pub_addr, ch1, no1])
-comm1 = utils.sha3(s1)
-
-choice2 = 0x0
-nonce2 = 0x01
-ch2 = ''.join(map(chr, tobytearr(choice2, 32)))
-no2 = ''.join(map(chr, tobytearr(nonce2, 32)))
-print("Player one chooses {} which is: {}\n").format(choice2, choice[choice2])
 
 k1_pub_addr_hex = utils.encode_hex(tester.k1)
+k1_pub_addr = hex_to_byte_string(long(k1_pub_addr_hex.encode('hex'), 16))
 
-## Prepare and pad the address
-k1_pub_addr  = ''.join(map(chr, tobytearr(long(k1_pub_addr_hex.encode('hex'),16),32)))
+s0 = ''.join([k0_pub_addr, choice0_bs, nonce0_bs])
+s1 = ''.join([k1_pub_addr, choice1_bs, nonce1_bs])
 
-## Now use it for the commitment
-s2 = ''.join([k1_pub_addr, ch2, no2])
-comm2 = utils.sha3(s2)
+# commitments
+comm0 = utils.sha3(s0)
+comm1 = utils.sha3(s1)
 
-o = c.add_player(comm1, value=1000, sender=tester.k0)
-print("Player 0 Added: {}").format(o)
-
-o = c.add_player(comm2, value=1000, sender=tester.k1)
-print("Player 1 Added: {}\n").format(o)
-
-o = c.open(0x01,0x01, sender=tester.k0)
-print("Open for player 0: {}").format(o)
-
-o = c.open(0x00,0x01, sender=tester.k1)
-print("Open for player 1: {}\n").format(o)
-
+# TODO fix this comment
+# add players to the game
+o = c.add_player(comm0, value=1000, sender=tester.k0)
+o = c.add_player(comm1, value=1000, sender=tester.k1)
+o = c.open(0x01, 0x01, sender=tester.k0)
+o = c.open(0x00, 0x01, sender=tester.k1)
 # needed to move the blockchain at least 10 blocks so check can run
 s.mine(11)
-
 o = c.check(sender=tester.k1)
 print("Check says player {} wins\n").format(o)
-
 c.balance_check(sender=tester.k0)
